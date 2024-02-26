90c5ce1b3ff46291dea27502bf12757ec84317bb
Andres Mendez <amenrio@proton.me>
2024-02-26 15:15:09 +0100

    Changed to tokyonigth

--- lua/amenrio/plugins/colors.lua
+++ lua/amenrio/plugins/colors.lua
@@ -1,31 +1,39 @@
 return {
     {
         "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                style = "night",
-                terminal_colors = true,
-                styles = {
-                    comments = {italic = false},
-                    keywords = {italic = false},
-                    sidebars = "dark",
-                    floats = "dark",
-                },
-            })
-        end
+        lazy = false,
     },
     {
         "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({
-                theme = "dragon",
+        config = function ()
+            require("catppuccin").setup({
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    harpoon = true,
+                    illuminate = true,
+                    indent_blankline = {
+                        enabled = false,
+                        scope_color = "sapphire",
+                        colored_indent_levels = false,
+                    },
+                    mason = true,
+                    native_lsp = { enabled = true },
+                    notify = true,
+                    nvimtree = true,
+                    neotree = true,
+                    symbols_outline = true,
+                    telescope = true,
+                    treesitter = true,
+                    treesitter_context = true,
+                }
             })
+            -- vim.cmd.colorscheme("catppuccin-macchiato")
+
+            -- Temporary solution
+            for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+                vim.api.nvim_set_hl(0, group, {})
+            end
         end,
     },
-
 }
--- lua/amenrio/plugins/copilot.lua
+++ lua/amenrio/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return{
-    'github/copilot.vim'
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function ()
+        require("copilot").setup()
+        require("copilot_cmp").setup()
+    end,
 }
