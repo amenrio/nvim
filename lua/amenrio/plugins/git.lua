428e838c187a6eaca5d7f6b3c4a78362c0c445a3
Andres Mendez <amenrio@gmail.com>
2024-03-20 17:34:23 +0100

    added vim-fugitive and gitsigns

---
+++ lua/amenrio/plugins/git.lua
@@ -0,0 +1,37 @@
+return{
+    {
+        'tpope/vim-fugitive'
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+            { "<leader>gC", "<cmd>LazyGitConfig<cr>", desc = "LazyGit config" },
+            { "<leader>gcf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit current file"},
+            { "<leader>gF", "<cmd>LazyGitFilter<cr>", desc = "LazyGit filter"},
+            { "<leader>gcF", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit filter current file" },
+        },
+
+        config = function()
+            vim.g.lazygit_floating_window_scaling_factor = 0.9
+        end,
+
+    },
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end,
+    }
+}
