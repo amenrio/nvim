6fe4d5eef7dbbbb79db1ba968a737c726fc67b37
Andres Mendez <amenrio@gmail.com>
2024-04-02 17:34:58 +0200

    added harpoon:setup() call

--- lua/amenrio/plugins/harpoon.lua
+++ lua/amenrio/plugins/harpoon.lua
@@ -1,19 +1,20 @@
 return{
     "ThePrimeagen/harpoon",
+    lazy=false,
     branch = "harpoon2",
     config = function()
         local harpoon = require("harpoon")
 
-        local wk = require("which-key")
+        harpoon:setup()
 
-        wk.register({
-            a = { function() harpoon:list():append() end, "Add current file to harpoon buffer" },
-        }, { prefix = "<leader>" })
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
         vim.keymap.set('n', '<C-h>', function() harpoon:list():select(1) end)
         vim.keymap.set('n', '<C-j>', function() harpoon:list():select(2) end)
         vim.keymap.set('n', '<C-k>', function() harpoon:list():select(3) end)
         vim.keymap.set('n', '<C-l>', function() harpoon:list():select(4) end)
+
     end
 
 }
