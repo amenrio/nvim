0a37949d98d229eccdfb1aaba2568629585e8167
Andres Mendez <amenrio@gmail.com>
2024-04-19 12:50:44 +0200

    fixed mini.lua configuration

--- lua/amenrio/plugins/mini.lua
+++ lua/amenrio/plugins/mini.lua
@@ -7,19 +7,10 @@ return{
         end
     },
     {
-        'echasnovski/mini.surround',
+        'echasnovski/mini.comment',
         version = false,
         config = function ()
-            require('mini.surround').setup({
-                search_method = 'cover_or_nearest'
-            })
-        end
-    },
-    {
-        'echasnovski/mini.starter',
-        version = false,
-        config = function ()
-            require('mini.starter').setup()
+            require('mini.comment').setup()
         end
     },
     {
@@ -30,10 +21,19 @@ return{
         end
     },
     {
-        'echasnovski/mini.comment',
+        'echasnovski/mini.starter',
         version = false,
         config = function ()
-            require('mini.comment').setup()
+            require('mini.starter').setup()
         end
     },
+    {
+        'echasnovski/mini.surround',
+        version = false,
+        config = function ()
+            require('mini.surround').setup({
+                search_method = 'cover_or_nearest'
+            })
+        end
+    }
 }
