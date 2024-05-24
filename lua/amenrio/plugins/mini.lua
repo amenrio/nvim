46b046afed5a9955009533d29ac85585eaa710d7
Andres Mendez <amenrio@gmail.com>
2024-05-24 11:07:35 +0200

    formatted files

--- lua/amenrio/plugins/mini.lua
+++ lua/amenrio/plugins/mini.lua
@@ -1,32 +1,39 @@
 return {
-	{
-		"echasnovski/mini.comment",
-		version = false,
-		config = function()
-			require("mini.comment").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-		version = false,
-		config = function()
-			require("mini.indentscope").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.starter",
-		version = false,
-		config = function()
-			require("mini.starter").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.surround",
-		version = false,
-		config = function()
-			require("mini.surround").setup({
-				search_method = "cover_or_nearest",
-			})
-		end,
-	},
+    {
+        "echasnovski/mini.comment",
+        version = false,
+        config = function()
+            require("mini.comment").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.indentscope",
+        version = false,
+        config = function()
+            require("mini.indentscope").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.starter",
+        version = false,
+        config = function()
+            require("mini.starter").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.surround",
+        version = false,
+        config = function()
+            require("mini.surround").setup({
+                search_method = "cover_or_nearest",
+            })
+        end,
+    },
+    -- {
+    --     "echasnovski/mini.files",
+    --     version = false,
+    --     config = function()
+    --         require("mini.files").setup()
+    --     end,
+    -- }
 }
