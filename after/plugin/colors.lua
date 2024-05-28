ac8eb54a65248ae2c678b69b0f44f4c55d51efa1
Andres Mendez <amenrio@gmail.com>
2024-05-28 14:45:21 +0200

    updated huez and transparent windows

---
+++ after/plugin/colors.lua
@@ -0,0 +1,8 @@
+function MakeTransparent()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeNormalFloat", { bg = "none" })
+end
+
+MakeTransparent()
--- lua/amenrio/plugins/huez-nvim.lua
+++ lua/amenrio/plugins/huez-nvim.lua
@@ -1,8 +1,14 @@
 return {
 	"vague2k/huez.nvim",
 	-- if you want registry related features, uncomment this
-	-- import = "huez-manager.import"
+	import = "huez-manager.import",
 	config = function()
 		require("huez").setup({})
+		local pickers = require("huez.pickers")
+
+		vim.keymap.set("n", "<leader>co", pickers.themes, {})
+		vim.keymap.set("n", "<leader>cof", pickers.favorites, {})
+		vim.keymap.set("n", "<leader>col", pickers.live, {})
+		vim.keymap.set("n", "<leader>coe", pickers.ensured, {})
 	end,
 }
